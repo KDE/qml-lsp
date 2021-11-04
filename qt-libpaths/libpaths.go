a9685218c16a2b1f74e7575a6e330e80dcfda0b3
Jan Blackquill <uhhadd@gmail.com>
2021-11-03 23:19:25 -0400

    feat: remove need for qt dep

--- qt-libpaths/libpaths.go
+++ qt-libpaths/libpaths.go
@@ -1,20 +1,47 @@
 package libpaths
 
-/*
-#cgo LDFLAGS: -Lutil/_build -lpaths
-#include "util/libpaths.h"
-*/
-import "C"
 import (
+	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
+
+func find(b []byte) string {
+	for _, ln := range strings.Split(string(b), "\n") {
+		if strings.HasPrefix(ln, "QT_INSTALL_QML:") {
+			return strings.TrimPrefix(ln, "QT_INSTALL_QML:")
+		}
+	}
+	return ""
+}
+
+func get() (string, error) {
+	var err error
+	var output []byte
+	for _, it := range []string{"qmake", "qmake-qt5"} {
+		output, err = exec.Command(it, "-query").Output()
+		if err != nil {
+			continue
+		}
+		break
+	}
+	if err != nil {
+		return "", err
+	}
+	return find(output), err
+}
 
 func Paths() []string {
 	env := strings.FieldsFunc(os.Getenv("QML2_IMPORT_PATH"), func(r rune) bool {
 		return r == ':'
 	})
-	qt := C.GoString(C.getLibraryPaths())
+	qt, err := get()
+	if err != nil {
+		panic("failed to get qt: " + err.Error())
+	}
+
+	fmt.Println(append(env, qt))
 
 	return append(env, qt)
 }
--- qt-libpaths/util/libpaths.cc
+++
@@ -1,9 +0,0 @@
-#include <QtCore/qlibraryinfo.h>
-#include "libpaths.h"
-
-static std::string it = QLibraryInfo::location(QLibraryInfo::Qml2ImportsPath).toStdString();
-
-const char* getLibraryPaths()
-{
-    return it.c_str();
-}
