fb560c1b218725334e5c9f8cd97def538fe4042c
Jan Blackquill <uhhadd@gmail.com>
2021-11-03 23:36:07 -0400

    feat: add shell script for static build

---
+++ build-static.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+export PATH="$PWD/wrappers:$PATH"
+export CGO_ENABLED=1
+export GOOS=linux
+export GOARCH=amd64
+export CC=/usr/local/musl/bin/musl-gcc
+
+go build --ldflags '-linkmode external -extldflags "-static"' -o qml-lsp-static
